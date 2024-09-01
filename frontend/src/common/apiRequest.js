477de10f0a4d2ad56f4d83fc8be6ca082b20bbbb
Vaibhav Darvekar <vabdarwekar00@gmail.com>
2024-09-01 18:47:54 +0530

    update

--- frontend/src/common/apiRequest.js
+++ frontend/src/common/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api", // Replace with your API base URL
+  baseURL: "https://notesapp-3av7.onrender.com/api", // Replace with your API base URL
   headers: {
     "Content-Type": "application/json",
   },
